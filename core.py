6a34a88beeb4a9e587942c7cb232adc081055044
praveenmarish <60656910+praveenmarish@users.noreply.github.com>
2020-02-29 21:40:37 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,43 +1,39 @@
 from tkinter import *
 from tkinter import messagebox
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
     
-def search(browser,product):
+def product_search(browser,product):
     
     print (product)
     
-    def product_search(driver):
-         
-        if(str(radio.get())==0):
+    def search():
+        #print("entered")
+        #print(product) 
                   
-            pass
-                  
-        elif (radio.get()==1):
-            matched_elements = driver.get("https://www.youtube.com")
+        if (radio.get()==1):
+            matched_elements = browser.get("https://www.flipkart.com")
+            soup=BeautifulSoup(browser.page_source,'html.parser')
+            print(soup)
             pass
         elif (radio.get()==2):
-            matched_elements = driver.get("https://www.youtube.com")             
+            matched_elements = browser.get("https://www.amazon.com") 
+            soup=BeautifulSoup(browser.page_source,'html.parser')
+            print(soup)
             pass
-    
-    def set_browser():
-        #print(str(radio.get()))
-        
-        if (browser=="google crome"):
-            browserdriver = webdriver.Chrome('chromedriver')
-            product_search(browserdriver)
-            pass
+        else:
+            ans=messagebox.askquestion("Confirm","Select a E-commerse site or did you give a url?\neg:https://www.site_name.com")  
+            if (ans=="yes"):
+                matched_elements = browser.get(product)
+                tk.withdraw()
+                pass
             
-        elif (browser=="microsoft edge"):
-            browserdriver = webdriver.Edge()
-            product_search(browserdriver)
+            else:
+                pass
+            #matched_elements = driver.get("https://"++)
             pass
-            
-        elif (browser=="mozilla firefox"):
-            browserdriver = webdriver.Firefox()
-            product_search(browserdriver)
-            pass 
             
         
 
@@ -53,11 +49,31 @@ def search(browser,product):
     r2=Radiobutton(tk,text="amazon",variable=radio,value=2)
     r1.pack(anchor=W)
     r2.pack(anchor=W)
-    b1=Button(tk,text="Ok",width=9,command=set_browser)
+    b1=Button(tk,text="Ok",width=9,command=search)
     b1.pack()
     tk.mainloop()
     pass
     
+        
+        
+def set_browser(browser,product):
+    #print(str(radio.get()))
+    
+    if (browser=="google crome"):
+        browserdriver = webdriver.Chrome('chromedriver')
+        product_search(browserdriver,product)
+        pass
+           
+    elif (browser=="microsoft edge"):
+        browserdriver = webdriver.Edge()
+        product_search(browserdriver,product)
+        pass
+              
+    elif (browser=="mozilla firefox"):
+        browserdriver = webdriver.Firefox()
+        product_search(browserdriver,product)
+        pass 
+            
         
         
 def compare():
@@ -67,3 +83,4 @@ def send_mail():
     
     pass
 
+set_browser("google crome","https://www.amazon.com/")
