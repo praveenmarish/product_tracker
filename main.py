214e89da30ed37a9baebe5bfc4cdc9a806e4c348
praveenmarish <60656910+praveenmarish@users.noreply.github.com>
2020-03-05 22:27:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,89 @@
-# main design
+#main design
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from core import set_browser, compare, send_mail,save_link
+from search import Search_Design
+from save import Save_Design
+from link import Links_Design
+from core import operations
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(549, 445)
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+class Ui_MainWindow(object):
+
+    def initiate(self):
+        self.load=operations()
+        self.load.Get_Data(str(self.comboBox.currentText()),self.lineEdit.text(),self.lineEdit_2.text(),self.lineEdit_3.text(),self.lineEdit_4.text())
+
+    def link_window(self):
+        self.initiate()
+        self.window=QtWidgets.QMainWindow()
+        self.ui=Links_Design()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+
+    def search_window(self):
+        self.initiate()
+        self.load.set_browser()
+        self.window=QtWidgets.QMainWindow()
+        self.ui=Search_Design()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+
+    def save_window(self):
+        self.initiate()
+        self.window=QtWidgets.QMainWindow()
+        self.ui=Save_Design()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+
+    def compare(self):
+        self.initiate()
+        self.load.compare()
+        #self.load.set_browser()
+        #self.load.amaz_search()
+        #self.load.flip_search()
+
+
+    def clear_data(self):
+        self.lineEdit.clear()
+        self.lineEdit_2.clear()
+        self.lineEdit_3.clear()
+        self.lineEdit_4.clear()
+
+
+    def close(self):
+        QtCore.QCoreApplication.instance().quit()
+
+
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(549, 446)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(180, 110, 181, 21))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(12)
         self.lineEdit.setFont(font)
         self.lineEdit.setObjectName("lineEdit")
-        self.label1 = QtWidgets.QLabel(Dialog)
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(300, 380, 75, 23))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_7.setFont(font)
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(440, 310, 75, 23))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_5.setFont(font)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.label1 = QtWidgets.QLabel(self.centralwidget)
         self.label1.setGeometry(QtCore.QRect(40, 110, 111, 21))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -26,7 +93,64 @@ class Ui_Dialog(object):
         self.label1.setFont(font)
         self.label1.setTextFormat(QtCore.Qt.PlainText)
         self.label1.setObjectName("label1")
-        self.label = QtWidgets.QLabel(Dialog)
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(40, 310, 111, 21))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(40, 210, 111, 21))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(440, 110, 75, 23))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(440, 210, 75, 23))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(180, 210, 181, 21))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(180, 310, 181, 21))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        self.lineEdit_4.setFont(font)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(180, 260, 181, 21))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        self.lineEdit_3.setFont(font)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(170, 380, 75, 23))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_6.setFont(font)
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 20, 531, 41))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -37,10 +161,16 @@ class Ui_Dialog(object):
         self.label.setTextFormat(QtCore.Qt.RichText)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(440, 110, 75, 23))
-        self.pushButton.setObjectName("pushButton")
-        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(180, 160, 181, 22))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        self.comboBox.setFont(font)
+        self.comboBox.setCurrentText("")
+        self.comboBox.setMaxVisibleItems(3)
+        self.comboBox.setObjectName("comboBox")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(40, 160, 111, 21))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -49,31 +179,13 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.comboBox = QtWidgets.QComboBox(Dialog)
-        self.comboBox.setGeometry(QtCore.QRect(180, 160, 181, 23))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(12)
-        self.comboBox.setFont(font)
-        self.comboBox.setCurrentText("")
-        self.comboBox.setMaxVisibleItems(3)
-        self.comboBox.setObjectName("comboBox")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(440, 160, 75, 23))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_3.setGeometry(QtCore.QRect(440, 210, 75, 23))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(40, 210, 111, 21))
         font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(Dialog)
+        font.setPointSize(10)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setGeometry(QtCore.QRect(40, 260, 111, 21))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -82,129 +194,64 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(180, 210, 181, 21))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(12)
-        self.lineEdit_2.setFont(font)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_3.setGeometry(QtCore.QRect(180, 260, 181, 21))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(12)
-        self.lineEdit_3.setFont(font)
-        self.lineEdit_3.setObjectName("lineEdit_3")
-        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_4.setGeometry(QtCore.QRect(440, 260, 75, 23))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_4.setFont(font)
         self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_5 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_5.setGeometry(QtCore.QRect(440, 310, 75, 23))
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.label_5 = QtWidgets.QLabel(Dialog)
-        self.label_5.setGeometry(QtCore.QRect(40, 310, 111, 21))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_5.setFont(font)
-        self.label_5.setObjectName("label_5")
-        self.lineEdit_4 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_4.setGeometry(QtCore.QRect(180, 310, 181, 21))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(12)
-        self.lineEdit_4.setFont(font)
-        self.lineEdit_4.setObjectName("lineEdit_4")
-        self.pushButton_6 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_6.setGeometry(QtCore.QRect(170, 380, 75, 23))
-        self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_7 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_7.setGeometry(QtCore.QRect(300, 380, 75, 23))
-        self.pushButton_7.setObjectName("pushButton_7")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-        pass
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.lineEdit.setPlaceholderText(_translate("Dialog", "Enter product name/URL"))
-        self.label1.setText(_translate("Dialog", "Product name:"))
-        self.label.setText(_translate("Dialog", "PRODUCT TRACKER"))
-        self.pushButton.setText(_translate("Dialog", "search"))
-        self.label_2.setText(_translate("Dialog", "Select browser:"))
-        self.pushButton_2.setText(_translate("Dialog", "compare"))
-        self.pushButton_3.setText(_translate("Dialog", "save link"))
-        self.label_3.setText(_translate("Dialog", "Maill id:"))
-        self.label_4.setText(_translate("Dialog", "Password:"))
-        self.lineEdit_2.setPlaceholderText(_translate("Dialog", "Enter maill id "))
-        self.lineEdit_3.setPlaceholderText(_translate("Dialog", "Enter password"))
-        self.pushButton_4.setText(_translate("Dialog", "show links"))
-        self.pushButton_5.setText(_translate("Dialog", "send mail"))
-        self.label_5.setText(_translate("Dialog", "Maill id:"))
-        self.lineEdit_4.setPlaceholderText(_translate("Dialog", "Enter target maill id "))
-        self.pushButton_6.setText(_translate("Dialog", "clear data"))
-        self.pushButton_7.setText(_translate("Dialog", "close"))
-        
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Enter name of a product"))
+        self.pushButton_7.setText(_translate("MainWindow", "Close"))
+        self.pushButton_5.setText(_translate("MainWindow", "Send maill"))
+        self.label1.setText(_translate("MainWindow", "Product name:"))
+        self.label_5.setText(_translate("MainWindow", "Maill id:"))
+        self.label_3.setText(_translate("MainWindow", "Maill id:"))
+        self.pushButton.setText(_translate("MainWindow", "Search"))
+        self.pushButton_3.setText(_translate("MainWindow", "Save link"))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Enter maill id "))
+        self.lineEdit_4.setPlaceholderText(_translate("MainWindow", "Enter target maill id "))
+        self.lineEdit_3.setPlaceholderText(_translate("MainWindow", "Enter password"))
+        self.pushButton_6.setText(_translate("MainWindow", "Clear data"))
+        self.label.setText(_translate("MainWindow", "PRODUCT TRACKER"))
+        self.label_2.setText(_translate("MainWindow", "Select browser:"))
+        self.pushButton_2.setText(_translate("MainWindow", "Compare"))
+        self.label_4.setText(_translate("MainWindow", "Password:"))
+        self.pushButton_4.setText(_translate("MainWindow", "Show link"))
+
+
         self.comboBox.addItem("google crome")
         self.comboBox.addItem("microsoft edge")
         self.comboBox.addItem("mozilla firefox")
-        #self.comboBox.addItem("safari")
-        
-        self.pushButton.clicked.connect(self.search)
+
+
+        self.pushButton.clicked.connect(self.search_window)
         self.pushButton_2.clicked.connect(self.compare)
-        self.pushButton_3.clicked.connect(self.save_link)
-        self.pushButton_4.clicked.connect(self.show_link)
-        self.pushButton_5.clicked.connect(self.send_mail)
+        self.pushButton_3.clicked.connect(self.save_window)
+        self.pushButton_4.clicked.connect(self.link_window)
+        self.pushButton_5.clicked.connect(self.link_window)
         self.pushButton_6.clicked.connect(self.clear_data)
         self.pushButton_7.clicked.connect(self.close)
-        pass
-    
-    
-    def search(self):
-        self.url=""
-        self.url=set_browser(str(self.comboBox.currentText()),self.lineEdit.text())
-        pass
-    
-    def compare(self):
-        self.url1=""
-        self.url2=""
-        self.url1,self.url2=compare(str(self.comboBox.currentText()),self.lineEdit.text())
-        pass
-    
-    def save_link(self):
-        save_link(self.url,self.url1,self.url2)
-        pass
-    
-    def show_link(self):
-        
-        pass
-    
-    def send_mail(self):
-        send_mail()
-        pass
-    
-    def clear_data(self):
-        self.lineEdit.clear()
-        self.lineEdit_2.clear()
-        self.lineEdit_3.clear()
-        self.lineEdit_4.clear()
-        pass
-    
-    def close(self):
-        QtCore.QCoreApplication.instance().quit()
-        pass
 
+
+
+    
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
