279b4843f9072554c6cfa6182e3ad050f1e015a4
praveenmarish <60656910+praveenmarish@users.noreply.github.com>
2020-03-07 20:03:42 +0530

    Update YesorNo.py

--- YesorNo.py
+++ YesorNo.py
@@ -1,35 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
-#from PyQt5.QtGui import QIcon
-#from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QWidget, QMessageBox
 from core import operations
 
 class App(QWidget):
 
-    def __init__(self):
+    def __init__(self,load):
+        self.load=load
         super().__init__()
-        self.title = 'PyQt5 messagebox - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 320
-        self.height = 200
         self.initUI()
         
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
 
         buttonReply = QMessageBox.question(self, 'Message', "Do you like to open browser?\nWithout search a product", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
         if buttonReply == QMessageBox.Yes:
-            #self.operations.set_browser()
-            self.load=operations()
             self.load.set_browser()         
         
-            
-
-        #self.show()
-"""       
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_()) """
