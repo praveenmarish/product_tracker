fd894731226ee628e83ad193cd07f7b7be67bcdb
praveenmarish <60656910+praveenmarish@users.noreply.github.com>
2020-03-10 21:23:46 +0530

    Update core.py

--- core.py
+++ core.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import os
 from selenium.webdriver.common.keys import Keys
 from pymongo import MongoClient 
+from warning import warnings
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 
@@ -12,150 +14,167 @@ class operations:
     def __init__(self,browser,product,emailid,password,targetid):
         self.url="https://www.flipkart.com"
         self.url1="https://www.amazon.in"
-        self.url3="https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3DOGB%26tab%3Drk1%26utm_medium%3Dapp&csig=AF-SEnb_VvdqbWeCSl9g%3A1583582267&flowName=GlifWebSignIn&flowEntry=AddSession"
+        self.url3="https://accounts.google.com/AccountChooser/identifier?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AddSession"
         self.browser=browser
         self.product=product
         self.emailid=emailid
         self.password=password
         self.targetid=targetid
-        self.conn = MongoClient("mongodb://localhost:27017/")
-        self.db = self.conn.database 
-        self.collection = self.db.sites
-        self.cursor = self.collection.find()
+        try:
+            self.conn = MongoClient("mongodb://localhost:27017/")
+            self.db = self.conn.database 
+            self.collection = self.db.sites
+            self.cursor = self.collection.find()
+
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on connecting to database")
         
         
     
     def save(self,product,url):
+        try:
+            product_entry = {
+                "product_name":product, 
+                "URL":url
+                } 
 
-        product_entry = {
-            "product_name":product, 
-            "URL":url
-            } 
-
-        self.collection.insert_one(product_entry) 
+            self.collection.insert_one(product_entry)
+        
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on database")
             
   
 
     def show_link(self):
-        product_name=""
-        url=""
-        for record in self.cursor:
-            for key,value in record.items():
-                for i in range (2):
-                    if (key=="product_name"):
-                        product_name=value
-                    elif (key=="URL"):
-                        url=value
-            if (product_name!="" and url!=""):
-                return product_name,url
+        try:
+            product_name=""
+            url=""
+            for record in self.cursor:
+                for key,value in record.items():
+                    for _i in range (2):
+                        if (key=="product_name"):
+                            product_name=value
+                        elif (key=="URL"):
+                            url=value
+                if (product_name!="" and url!=""):
+                    return product_name,url
+
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on database")
 
 
     def delete(self,product,url):
-          
-        # Created or Switched to collection names: my_gfg_collection 
-        self.collection = self.db.sites 
-  
-        self.collection.delete_one = {
-            "product_name":product, 
-            "URL":url
-            } 
+        try:
+            self.collection = self.db.sites 
+            self.collection.delete_one = {
+                "product_name":product, 
+                "URL":url
+                } 
+
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on database")
 
 
 
         
         
     def send_mail(self,product,url):
-        self.browserdriver.get(self.url3)
-        user_name_field = self.browserdriver.find_element_by_xpath("""//*[@id="identifierId"]""")
-        user_name_field.send_keys(self.emailid)
-        u_form_next = self.browserdriver.find_element_by_class_name('CwaK9')
-        u_form_next.click()
-        #password_field=self.browserdriver.find_element_by_class_name('Xb9hP')
-        #password_field.send_keys(self.password)
-        self.browserdriver.implicitly_wait(30)
-        password_field=self.browserdriver.find_element_by_xpath("""//*[@id="password"]/div[1]/div/div[1]/input""")
-        #password_field=self.browserdriver.find_elements_by_name('password')
-        #password_field.clear()
-        password_field.send_keys(self.password)
-        p_form_next=self.browserdriver.find_element_by_class_name('CwaK9')
-        p_form_next.click()
-
-        self.browserdriver.implicitly_wait(2000)
-        compose=self.browserdriver.find_element_by_class_name('T-I J-J5-Ji T-I-KE L3')
-        compose.click()
-        self.browserdriver.implicitly_wait(200)
-        to_address=self.browserdriver.find_elements_by_xpath("""//*[@id=":pk"]""")
-        to_address.send_keys(self.targetid)
-        subject=self.browserdriver.find_elements_by_xpath("""//*[@id=":p2"]""")
-        subject.send_keys(product)
-        condent=self.browserdriver.find_elements_by_xpath("""//*[@id=":q7"]""")
-        condent.send_keys(url)
-
-        self.browserdriver.implicitly_wait(200)
-        send=self.browserdriver.find_element_by_class_name('T-I J-J5-Ji aoO v7 T-I-atl L3')
-        send.click()
+        try:
+            self.browserdriver.get(self.url3)
+            user_name_field = self.browserdriver.find_element_by_xpath("""//*[@id="identifierId"]""")
+            user_name_field.send_keys(self.emailid)
+            u_form_next = self.browserdriver.find_element_by_class_name('CwaK9')
+            u_form_next.click()
+            self.browserdriver.implicitly_wait(40)
+            password_field=self.browserdriver.find_element_by_xpath("""//*[@id="password"]/div[1]/div/div[1]/input""")
+            password_field.send_keys(self.password)
+            p_form_next=self.browserdriver.find_element_by_class_name('CwaK9')
+            p_form_next.click()
+            self.browserdriver.implicitly_wait(40)
+            compose=self.browserdriver.find_element_by_class_name('T-I J-J5-Ji T-I-KE L3')
+            compose.click()
+            self.browserdriver.implicitly_wait(40)
+            to_address=self.browserdriver.find_elements_by_xpath("""//*[@id=":pk"]""")
+            to_address.send_keys(self.targetid)
+            subject=self.browserdriver.find_elements_by_xpath("""//*[@id=":p2"]""")
+            subject.send_keys(product)
+            condent=self.browserdriver.find_elements_by_xpath("""//*[@id=":q7"]""")
+            condent.send_keys(url)
+            self.browserdriver.implicitly_wait(40)
+            send=self.browserdriver.find_element_by_class_name('T-I J-J5-Ji aoO v7 T-I-atl L3')
+            send.click()
+        
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on sending mail")
 
        
         
 
             
     def compare(self):
-    
-        if (self.browser=="google crome"):
-            self.set_browser()
-            self.flip_search()
-            self.browserdriver.execute_script('''window.open("about:blank", "_blank");''')
-            self.browserdriver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
-            self.browserdriver.switch_to.window(self.browserdriver.window_handles[1])
-            self.amaz_search()
-            
-            
+
+        try:
+            if (self.browser=="google crome"):
+                self.set_browser()
+                self.flip_search()
+                self.browserdriver.execute_script('''window.open("about:blank", "_blank");''')
+                self.browserdriver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+                self.browserdriver.switch_to.window(self.browserdriver.window_handles[1])
+                self.amaz_search()
                
-        elif (self.browser=="microsoft edge"):
-            self.set_browser()
-            self.flip_search()
-            self.browserdriver.execute_script('''window.open("about:blank", "_blank");''')
-            self.browserdriver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
-            self.browserdriver.switch_to.window(self.browserdriver.window_handles[1])
-            self.amaz_search()
+            elif (self.browser=="microsoft edge"):
+                self.set_browser()
+                self.flip_search()
+                self.browserdriver.execute_script('''window.open("about:blank", "_blank");''')
+                self.browserdriver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+                self.browserdriver.switch_to.window(self.browserdriver.window_handles[1])
+                self.amaz_search()
             
-            
-                  
-        elif (self.browser=="mozilla firefox"):
-            self.set_browser()
-            self.flip_search()
-            self.browserdriver.execute_script('''window.open("about:blank", "_blank");''')
-            self.browserdriver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
-            self.browserdriver.switch_to.window(self.browserdriver.window_handles[1])
-            self.amaz_search()
+            elif (self.browser=="mozilla firefox"):
+                self.set_browser()
+                self.flip_search()
+                self.browserdriver.execute_script('''window.open("about:blank", "_blank");''')
+                self.browserdriver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+                self.browserdriver.switch_to.window(self.browserdriver.window_handles[1])
+                self.amaz_search()
+
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on searching")
             
             
             
             
     def amaz_search(self):
-        self.browserdriver.get(self.url1)      
-        search_field=self.browserdriver.find_element_by_xpath("""//*[@id="twotabsearchtextbox"]""")
-        search_field.send_keys(self.product)
-        search_field.submit()
+        try:
+            self.browserdriver.get(self.url1)
+            search_field=self.browserdriver.find_element_by_xpath("""//*[@id="twotabsearchtextbox"]""")
+            search_field.send_keys(self.product)
+            search_field.submit()
+
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on searching")
         
     
         
         
     def flip_search(self):
-        self.browserdriver.get(self.url)
         try:
+            self.browserdriver.get(self.url)
             login_pop = self.browserdriver.find_element_by_class_name('_29YdH8')
-            # Here .click function use to tap on desire elements of webpage
             login_pop.click()
-            
+            search_field = self.browserdriver.find_element_by_class_name('LM6RPg')
+            search_field.send_keys(self.product + '\n')
+
         except:
-            print("No pop-up")
-        
-        # Here I get search field id from driver
-        search_field = self.browserdriver.find_element_by_class_name('LM6RPg')
-        # Here .send_keys is use to input text in search field
-        search_field.send_keys(self.product + '\n')
-        
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on searching")
         
         
         
@@ -163,26 +182,24 @@ class operations:
     
     
     def set_browser(self):
-       
-        if (self.browser=="google crome"):
-            driver_path = os.path.join(os.getcwd(), 'chromedriver')
-            self.browserdriver = webdriver.Chrome(driver_path)
-            #self.product_search()
-            pass
+             
+        try:
+            if (self.browser=="google crome"):
+                driver_path = os.path.join(os.getcwd(), 'chromedriver')
+                self.browserdriver = webdriver.Chrome(driver_path)
         
            
-        elif (self.browser=="microsoft edge"):
-            driver_path = os.path.join(os.getcwd(), '')
-            self.browserdriver = webdriver.Edge(driver_path)
-            #self.product_search()
-            pass
+            elif (self.browser=="microsoft edge"):
+                driver_path = os.path.join(os.getcwd(), '')
+                self.browserdriver = webdriver.Edge(driver_path)
         
               
-        elif (self.browser=="mozilla firefox"):
-            driver_path = os.path.join(os.getcwd(), '')
-            self.browserdriver = webdriver.Firefox(driver_path)
-            #self.product_search()
-            pass
-        
-     
-    
+            elif (self.browser=="mozilla firefox"):
+                driver_path = os.path.join(os.getcwd(), '')
+                self.browserdriver = webdriver.Firefox(driver_path)
+
+        except:
+            self.window=QtWidgets.QMainWindow()
+            self.ui=warnings("Problem on opening browser")
+
+   
